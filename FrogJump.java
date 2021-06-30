79b47b91f04bd132ca3e7527c2048a4d323b174e
Koustubh Mokashi <koustubh.mokashi@gmail.com>
2021-06-30 14:47:05 +0530

    Create FrogJump.java

---
+++ FrogJump.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean canCross(int[] stones) {
+        final int n = stones.length;
+        
+        boolean[][] dp = new boolean[n][n + 1];
+        dp[0][1] = true;
+
+        for (int i = 1; i < n; ++i)
+            for (int j = 0; j < i; ++j) {
+                final int k = stones[i] - stones[j];
+                if (k <= n && dp[j][k]) {
+                    dp[i][k - 1] = true;
+                    dp[i][k] = true;
+                    dp[i][k + 1] = true;
+                }
+            }
+
+        for (int j = 0; j <= n; ++j)
+            if (dp[n - 1][j])
+                return true;
+
+        return false;  
+    }
+}
